eab4267a33b576c8b79d12d3d0d46f427af77b14
xutianyi <xutianyi1999@live.com>
2025-01-24 13:10:42 +0800

    t3

--- src/infer.rs
+++ src/infer.rs
@@ -404,9 +404,8 @@ impl <'a> SpeculativeCompletionsTargetSequenceSlots<'a> {
             if token != draft_tokens[draft_idx] {
                 let candidates = seq.sampler.get_candidates();
                 ensure!(candidates.sorted);
-                ensure!(self.n_candidates <= candidates.size);
 
-                let token_data_list = unsafe { &*slice_from_raw_parts(candidates.data, self.n_candidates) };
+                let token_data_list = unsafe { &*slice_from_raw_parts(candidates.data, min(self.n_candidates, candidates.size)) };
 
                 for td in token_data_list {
                     if td.id == draft_tokens[draft_idx].0 {
