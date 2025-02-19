98f1dcf09f54981b24fae0c3fec674abc54b862f
xutianyi <xutianyi1999@live.com>
2025-02-19 15:16:52 +0800

    update

--- src/infer.rs
+++ src/infer.rs
@@ -424,7 +424,7 @@ impl <'a> SpeculativeCompletionsTargetSequenceSlots<'a> {
                                             ctx.clear_kv_cache_seq(Some(id as u32), Some(sub_seq_len as u32), None)?;
                                         }
 
-                                        if sub_seq_len < token_list.len() - 1 {
+                                        if sub_seq_len == token_list.len() - 1 {
                                             continue;
                                         }
 
