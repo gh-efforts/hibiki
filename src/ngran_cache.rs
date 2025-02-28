ece58cc80c757b0832b0daf9261f55e5f3779166
xutianyi <xutianyi1999@live.com>
2025-02-28 14:48:58 +0800

    update

--- src/ngran_cache.rs
+++ src/ngran_cache.rs
@@ -1,27 +1,76 @@
-use llama_cpp_sys_2::HibikiCommonNgramCache;
+use llama_cpp_sys_2::{llama_token, HibikiCommonNgramCache};
 use anyhow::Result;
 
 pub struct NgranCache {
-    inner: *mut HibikiCommonNgramCache
+    nc_context: *mut HibikiCommonNgramCache,
+    nc_dynamic: *mut HibikiCommonNgramCache,
+    nc_static: *mut HibikiCommonNgramCache,
 }
 
 impl NgranCache {
     pub fn new() -> Self {
         unsafe {
-            let p = llama_cpp_sys_2::hibiki_common_ngram_cache_new();
-            Self { inner: p }
+            Self {
+                nc_context: llama_cpp_sys_2::hibiki_common_ngram_cache_new(),
+                nc_dynamic: llama_cpp_sys_2::hibiki_common_ngram_cache_new(),
+                nc_static: llama_cpp_sys_2::hibiki_common_ngram_cache_new(),
+            }
         }
     }
 
-    pub fn update(&mut self) {
+    pub fn update(
+        &mut self,
+        ngram_min: i32,
+        ngram_max: i32,
+        inp_data: &[llama_token],
+        nnew: i32
+    ) {
+        unsafe {
+            llama_cpp_sys_2::hibiki_common_ngram_cache_update(
+                self.nc_context,
+                ngram_min,
+                ngram_max,
+                inp_data.as_ptr(),
+                inp_data.len() as i32,
+                nnew,
+                false
+            )
+        }
+    }
 
+    pub fn draft(
+        &self,
+        inp_data: &[llama_token],
+        draft: &mut [llama_token],
+        ngram_min: i32,
+        ngram_max: i32,
+    ) -> usize {
+        let mut draft_len = 0;
+
+        unsafe {
+            llama_cpp_sys_2::hibiki_common_ngram_cache_draft(
+                inp_data.as_ptr(),
+                inp_data.len() as i32,
+                draft.as_mut_ptr(),
+                &mut draft_len,
+                draft.len() as i32,
+                ngram_min,
+                ngram_max,
+                self.nc_context,
+                self.nc_dynamic,
+                self.nc_static
+            )
+        }
+        draft_len as usize
     }
 }
 
 impl Drop for NgranCache {
     fn drop(&mut self) {
         unsafe {
-            llama_cpp_sys_2::hibiki_common_ngram_cache_free(self.inner);
+            llama_cpp_sys_2::hibiki_common_ngram_cache_free(self.nc_context);
+            llama_cpp_sys_2::hibiki_common_ngram_cache_free(self.nc_dynamic);
+            llama_cpp_sys_2::hibiki_common_ngram_cache_free(self.nc_static);
         }
     }
 }
